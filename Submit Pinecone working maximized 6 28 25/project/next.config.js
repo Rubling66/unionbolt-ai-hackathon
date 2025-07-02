d98186576d4032c05d1c66f25dcdc9189f11a8c0
Rubling66 <ReubenmCole@Gmail.com>
2025-07-01 23:39:48 -0400

    Update next.config.js

--- Submit Pinecone working maximized 6 28 25/project/next.config.js
+++ Submit Pinecone working maximized 6 28 25/project/next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
